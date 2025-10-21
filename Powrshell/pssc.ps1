adc1e6d03d18c3c5e6240698d641716e1c154b66
Joygit <joy.chakraborty.3520@gmail.com>
2025-10-22 00:00:04 +0530

    Added 1st file

---
+++ Powrshell/pssc.ps1
@@ -0,0 +1,9 @@
+# to use help command using examples.
+Get-Help Get-Service -Examples
+Get-Help Get-Service -Full
+Get-Help Get-Service -Detailed
+Get-Verb
+Get-Verb | Measure-Object
+Get-Process
+Get-Service
+help *service*
